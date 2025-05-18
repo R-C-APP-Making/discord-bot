ab1f0d174c102938bce47982d67990503c5f5421
Rickory2021 <42385877+Rickory2021@users.noreply.github.com>
2025-05-17 23:05:11 -0400

    Update eslint.config.js

--- bots/v1-bot/eslint.config.js
+++ bots/v1-bot/eslint.config.js
@@ -1,34 +1,33 @@
 // eslint.config.js
 const js = require('@eslint/js');
 const pluginPrettier = require('eslint-plugin-prettier');
+const globals = require('globals');
 
 module.exports = [
   // 1) ESLint’s built-in “recommended” JS rules
   js.configs.recommended,
 
-  // 2) Prettier integration
+  // 2) Your Prettier + custom rules, plus Node globals
   {
-    // register the plugin under the “prettier” name
     plugins: { prettier: pluginPrettier },
-
-    // spread in all of plugin-prettier’s recommended rules,
-    // then override prettier/prettier with your inline opts
     rules: {
       ...pluginPrettier.configs.recommended.rules,
       'prettier/prettier': ['error', { semi: true, singleQuote: true }],
-      // your custom overrides:
       'no-console': 'warn',
       eqeqeq: 'error',
     },
 
     languageOptions: {
-      env: {
-        node: true, // enable Node.js global vars (require, module, console, etc.)
-        es2021: true, // enable ES2021 globals like Promise
+      parserOptions: {
+        ecmaVersion: 12, // ES2021 syntax
+        sourceType: 'module', // if you ever use import/exports
       },
 
-      ecmaVersion: 12, // ES2021
-      sourceType: 'module', // import/export
+      // <-- enable Node & ES2021 built-ins here
+      globals: {
+        ...globals.node, // require, module, console, process, __dirname, etc.
+        ...globals.es2021, // Promise, etc.
+      },
     },
   },
 ];

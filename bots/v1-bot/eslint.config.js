b5894fc08e936c1541a4e2381fec8d0302ac473f
Rickory2021 <42385877+Rickory2021@users.noreply.github.com>
2025-05-17 22:13:36 -0400

    Changed to recommended config

--- bots/v1-bot/eslint.config.js
+++ bots/v1-bot/eslint.config.js
@@ -3,27 +3,26 @@ const js = require('@eslint/js');
 const pluginPrettier = require('eslint-plugin-prettier');
 
 module.exports = [
-  // 1) Core “recommended” JS rules
+  // 1) Core ESLint rules
   js.configs.recommended,
 
-  // 2) Add Node.js globals (console, process, __dirname, etc.)
-  js.configs.node,
-
-  // 3) Prettier integration + your overrides
+  // 2) Prettier + custom rules + globals
   {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      // Manually expose console/process as globals:
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+      },
+    },
     plugins: { prettier: pluginPrettier },
     rules: {
-      // Pull in Prettier’s recommended rules:
       ...pluginPrettier.configs.recommended.rules,
-      // Then override its core rule:
       'prettier/prettier': ['error', { semi: true, singleQuote: true }],
-      // Your custom ESLint rules:
       'no-console': 'warn',
       eqeqeq: 'error',
-    },
-    languageOptions: {
-      ecmaVersion: 12, // ES2021
-      sourceType: 'module', // import/export
     },
   },
 ];
